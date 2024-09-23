769784d25004cb12f7b80c323164b1d71b850ce0
Aaron Helton <helton@un.org>
2024-09-23 18:03:43 -0400

    first commit

---
+++ pages/build-character.vue
@@ -0,0 +1,15 @@
+<script lang="js">
+
+</script>
+<script setup lang="js">
+const state = reactive({
+    characterName: undefined,
+    characterKindred: undefined,
+    characterClass: undefined,
+})
+</script>
+<template>
+    <UContainer>
+
+    </UContainer>
+</template>
---
+++ pages/build-class.vue
@@ -0,0 +1,17 @@
+<script lang="js"></script>
+<script setup lang="js">
+const state = reactive({
+    className: undefined,
+    rac: undefined,
+    primeScores: undefined,
+    hitDice: undefined,
+    hitDiceCap: undefined,
+    maxLevels: undefined,
+    armorAllowed: undefined,
+    weaponsAllowed: undefined,
+
+})
+</script>
+<template>
+
+</template>
